ffe7815400eb7c45af6921a82116c036734217dc
Pandhari <pandhari2527@gmail.com>
2025-02-13 15:00:35 +0530

    removed datasets

---
+++ xlsx_to_csv.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# Load the Excel file
+df = pd.read_excel("/Users/pandhari/ai-diary-project/Data/diary_dataset.xlsx")
+
+# Save it as a CSV file
+df.to_csv("/Users/pandhari/ai-diary-project/Data/diary_dataset.csv", index=False)
+
+print("Conversion completed! CSV file saved successfully.")
